4df49d65fa068e8c5e68c6aa87395b98b4615935
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-11-06 13:04:06 +0800

    add icons

---
+++ lib/icons.ts
@@ -0,0 +1,7 @@
+export { FaRegUser as User, FaRegEdit as Edit } from "react-icons/fa";
+export { TbLockPassword as Password } from "react-icons/tb";
+export { HiOutlineFilter as Filter } from "react-icons/hi";
+export { CgMenuRight as Menu } from "react-icons/cg";
+export { LuSwitchCamera as Picture } from "react-icons/lu";
+export { RiDeleteBinLine as Delete } from "react-icons/ri";
+export { IoClose as Close } from "react-icons/io5";
