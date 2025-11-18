8944bf7aa4e755056c53aa1edb45415106fb46b4
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-11-18 14:17:43 +0800

    add login form

--- lib/imports.ts
+++ lib/imports.ts
@@ -1,10 +1,12 @@
 
 //UI Components
 export { default as Header } from '@/components/ui/Header';
+export { default as Login } from '@/components/ui/LoginForm';
 
 //Atoms
 export { default as Button } from '@/components/atoms/Buttton';
 export { default as Tabs } from '@/components/atoms/Tabs';
+export { default as InputField } from '@/components/atoms/InputField';
 
 
 //Data
