d40f527fb55b5a60c920ed4a4de0718b38a389c6
John Rey Luntayao <johnrey.luntayao23@gmail.com>
2025-11-21 17:40:04 +0800

    add popup cards

--- data/index.ts
+++ data/index.ts
@@ -11,3 +11,21 @@ export const memberLinks = [
   { id: 3, title: "My Attendance", href: "/my-attendance" },
   { id: 4, title: "My Account", href: "/my-account" },
 ];
+
+
+export const department = [
+  {
+    id: 1,
+    acronym: "COED",
+    name: "College of Education"
+  },
+]
+
+export const courses = [
+  {
+    id: 1,
+    acronym: "BSIT",
+    name: "Bachelor of Science in Information Technology",
+    department: "College of Education",
+  }
+]
