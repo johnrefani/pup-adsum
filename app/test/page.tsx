9bbc2abc5f047e2ce412f648a184fa800b394088
John Cloyd Refani <jhnrefani@gmail.com>
2025-11-28 16:24:31 +0800

    Added Session Information UI

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import SessionInformation from '@/components/ui/SessionInformation'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <SessionInformation mode="edit" />
+    </div>
+  )
+}
+
+export default page
