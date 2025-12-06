4138f4c983d5a234ff40d6d5186f596f71468bf2
John Cloyd Refani <jhnrefani@gmail.com>
2025-12-06 21:45:43 +0800

    Added Admin UI

---
+++ app/test/page.tsx
@@ -0,0 +1,15 @@
+import AdminList from '@/components/ui/AdminList'
+import CourseList from '@/components/ui/CourseList'
+import DepartmentList from '@/components/ui/DepartmentList'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+
+        <AdminList/>
+    </div>
+  )
+}
+
+export default page
